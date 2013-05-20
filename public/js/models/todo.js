5cbe59190088938ae7e3cd28885fe6965ad7cc4d
Andrew Newdigate <andrew@troupe.co>
2013-05-20 14:13:45 +0100

    Fixed issue where wrong attribute was being used for ID

--- public/js/models/todo.js
+++ public/js/models/todo.js
@@ -9,6 +9,8 @@ var app = app || {};
 
 	// Our basic **Todo** model has `title`, `order`, and `completed` attributes.
 	app.Todo = Backbone.Model.extend({
+		idAttribute: '_id',
+
 		// Default attributes for the todo
 		// and ensure that each todo created has `title` and `completed` keys.
 		defaults: {
