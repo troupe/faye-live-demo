5c0712d86f88c257253b0c31a90821ae8e5a4954
Mike Bartlett <mike.bartlett@gmail.com>
2013-05-21 14:26:10 +0100

    minor adjustments

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ var mongooseUtils = require('./lib/mongoose-utils');
 var Schema = mongoose.Schema;
 
 
+// The mongoose schema used by the Todo list demo
+var todoSchema = new Schema({
+    title: String,
+    completed: Boolean
+});
+
 // This method will send realtime notifications to the faye clients
 function notifyClients(method, model, url) {
 	bayeux.getClient().publish(url, {
@@ -17,13 +23,6 @@ function notifyClients(method, model, url) {
 		body: model.toJSON()
 	});
 }
-
-
-// The mongoose schema used by the Todo list demo
-var todoSchema = new Schema({
-    title: String,
-    completed: Boolean
-});
 
 // Attach events to the mongoose schema. Since the default mongoose middleware makes it difficult to
 // distinguish between a create and an update event, we use a small utility that helps us to do that.
